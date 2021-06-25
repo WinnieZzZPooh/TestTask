2288cd77d9b9b42a2b96339388f4d12ea78ae8bd
winnie_the_pooh <qwerwf>
2021-06-25 11:40:42 +0500

    added publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/test-task/'
+    : '/',
+
   configureWebpack: {
     resolve: {
       alias: {
